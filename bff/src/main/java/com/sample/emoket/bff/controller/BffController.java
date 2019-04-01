8676e9d332aed7f2aadd2bf7a52d29e2c28e689a
Yongjin Park <insight.emoket@gmail.com>
2019-04-01 14:14:54 +0900

    bff pom.xml dependencies 추가 및 Dockerfile 위치 변경/내용 수정

--- bff/src/main/java/com/sample/emoket/bff/controller/BffController.java
+++ bff/src/main/java/com/sample/emoket/bff/controller/BffController.java
@@ -4,9 +4,13 @@ import com.sample.emoket.bff.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("/api")
@@ -16,38 +20,100 @@ public class BffController {
 
     private static final Logger LOG = LoggerFactory.getLogger(BffController.class);
 
-
     // 스프링부트 백엔드 호출 서비스
-    @Value("${backend.spring.host}")
-    private String backendSpringHost;
+    @Value("${backend.spring.host}") private String backendSpringHost;
+    @Value("${backend.spring.port}") private String backendSpringPort;
 
-    @Value("${backend.spring.port}")
-    private String backendSpringPort;
+    // 노드 백엔드 호출 서비스
+    @Value("${backend.node.host}") private String backendNodeHost;
+    @Value("${backend.node.port}") private String backendNodePort;
 
+    // Greeting Service for Spring
     @GetMapping(path = "/hello/spring")
-    public String getGreetingMessageFromSpring() {
-        String url = "http://" + backendSpringHost + ":" + backendSpringPort + "/api/hello/spring";
-        LOG.info(url);
-        String resultMessage = restTemplate.getForObject(url, String.class);
-        LOG.info(resultMessage);
-        return resultMessage;
+    public ResponseEntity<String> getGreetingMessageFromSpring() {
+
+        // Spring Backend Service 명칭이 없는 경우, 추가 진행 없이 종료
+        if ( !Optional.ofNullable(backendSpringHost).filter(f -> !f.isEmpty()).isPresent() ) {
+            LOG.info("No Spring Backend Service");
+            return new ResponseEntity<>("No Spring Backend Service", HttpStatus.OK);
+        }
+
+        LOG.info("Call Spring Backend Service");
+
+        // 백엔드 호출
+        ResponseEntity<String> response = restTemplate.exchange(
+            "http://" + backendSpringHost + ":" + backendSpringPort + "/api/hello/spring",
+            HttpMethod.GET,
+            null,
+            String.class
+        );
+
+        if ( !Optional.ofNullable(response.getBody()).isPresent() ) {
+            LOG.info("No data found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        LOG.info(response.getBody());
+
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/{id}")
+    public ResponseEntity<String> getUserById(@PathVariable("id") long id) {
+
+        // Spring Backend Service 명칭이 없는 경우, 추가 진행 없이 종료
+        if ( !Optional.ofNullable(backendSpringHost).filter(f -> !f.isEmpty()).isPresent() ) {
+            LOG.info("No Spring Backend Service");
+            return new ResponseEntity<>("No Was Service", HttpStatus.OK);
+        }
+
+        LOG.info("Get User : " + id);
+
+        // 백엔드 호출
+        ResponseEntity<String> response = restTemplate.exchange(
+            "http://" + backendSpringHost + ":" + backendSpringPort + "/api/user/" + id,
+            HttpMethod.GET,
+            null,
+            String.class
+        );
+
+        if ( !Optional.ofNullable(response.getBody()).isPresent() ) {
+            LOG.info("No data found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
     }
 
 
-    // 노드 백엔드 호출 서비스
-    @Value("${backend.node.host}")
-    private String backendNodeHost;
-
-    @Value("${backend.node.port}")
-    private String backendNodePort;
-
+    // Greeting Service for Node
     @GetMapping(path = "/hello/node")
     public String getGreetingMessageFromNode() {
-        String url = "http://" + backendNodeHost + ":" + backendNodePort + "/api/hello/node";
-        LOG.info(url);
-        String resultMessage = restTemplate.getForObject(url, String.class);
-        LOG.info(resultMessage);
-        return resultMessage;
+
+        // Node Backend Service 명칭이 없는 경우, 추가 진행 없이 종료
+        if ( !Optional.ofNullable(backendNodeHost).filter(f -> !f.isEmpty()).isPresent() ) {
+            LOG.info("No Node Backend Service");
+            return "No Node Backend Service";
+        }
+
+        LOG.info("Call Node Backend Service");
+
+        // Node Backend Service 호출
+        ResponseEntity<String> response = restTemplate.exchange(
+            "http://" + backendNodeHost + ":" + backendNodePort + "/api/hello/node",
+            HttpMethod.GET,
+            null,
+            String.class
+        );
+
+        if ( !Optional.ofNullable(response.getBody()).isPresent() ) {
+            LOG.info("No data found");
+            return "No data found";
+        }
+
+        LOG.info(response.getBody());
+
+        return response.getBody();
     }
 
     @RequestMapping(path = "/user", method = RequestMethod.POST)
@@ -59,17 +125,5 @@ public class BffController {
         LOG.info(response.getBody());
         return response.getBody();
     }
-
-    @GetMapping(path = "/user/{id}")
-    public String passingGetUserById(@PathVariable("id") long id) {
-        LOG.info("id : " + id); // id : 2 ok
-        String url = "http://" + backendSpringHost + ":" + backendSpringPort + "/api/user/" + id;
-        LOG.info(url); // ok
-        String response = restTemplate.getForObject(url, String.class);
-        LOG.info(response);
-        return response;
-    }
-
-
 
 }
